577f9337bbb24ba116818c5469a6efd3e9826308
caius_kong <Caius_Kong@trendmicro.com>
2018-11-22 15:16:49 +0800

    直接执行，SHELL脚本中的export无法生效, 需要source init.sh

--- open_nmt/init.sh
+++ open_nmt/init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# just for nvidia-docker (ps、source init.sh)
+# just for nvidia-docker (ps、直接执行，SHELL脚本中的export无法生效, 需要source init.sh)
 export OPENNMT_HOME=/root/opennmt
 export LUA_PATH="$LUA_PATH;$OPENNMT_HOME/?.lua"
