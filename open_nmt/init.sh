516eaefd392c2cacdade845ce5d29182b9a29cfc
caius_kong <Caius_Kong@trendmicro.com>
2018-11-22 15:06:22 +0800

    Init.sh

--- open_nmt/init.sh
+++ open_nmt/init.sh
@@ -3,6 +3,3 @@
 # just for nvidia-docker
 export OPENNMT_HOME=/root/opennmt
 export LUA_PATH="$LUA_PATH;$OPENNMT_HOME/?.lua"
-
-# Make sure the container does not exit after executing the script
-tail -f /dev/null
