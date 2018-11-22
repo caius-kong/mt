adc38606d4739c5ea391fcb4661b7ee3a95bfd61
caius_kong <Caius_Kong@trendmicro.com>
2018-11-22 17:28:15 +0800

    open_nmt

--- open_nmt/release_model.sh
+++ open_nmt/release_model.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # It will generate a en-de-model_epoch13_10.10_release.t7 file
+# Note: if you training a model with GPU, you must use `-gpuid` when releasing
 # Warning: released models can no longer be used for training
 th $OPENNMT_HOME/tools/release_model.lua -model output/en-de-model_epoch13_10.10.t7
--- open_nmt/translate.sh
+++ open_nmt/translate.sh
@@ -2,4 +2,4 @@
 
 # Note1: if you training a model with GPU, you must use `-gpuid` when translating
 # Note2: you can use `tools/release_model.lua` to release model. A released model takes less space on disk and is compatible with both CPU and GPU translation.
-th $OPENNMT_HOME/translate.lua -model output/en-de-model_epoch13_10.10.t7 -src input/en-de/src-test.txt -output output/en-de-pred13.txt
+th $OPENNMT_HOME/translate.lua -model output/en-de-model_epoch13_10.10_release.t7 -src input/en-de/src-test.txt -output output/en-de-pred13.txt
