3cbbaf7245833aab38a95d91f1c2330dd9e2f7ce
caius_kong <Caius_Kong@trendmicro.com>
2018-11-22 17:06:38 +0800

    open_nmt

--- open_nmt/release_model.sh
+++ open_nmt/release_model.sh
@@ -2,4 +2,4 @@
 
 # It will generate a en-de-model_release.t7 file
 # Warning: The script need `-gpuid` and released models can no longer be used for training
-th $OPENNMT_HOME/tools/release_model.lua -model en-de-model.t7 -gpuid 1
+th $OPENNMT_HOME/tools/release_model.lua -model en-de-model_epoch13_10.10.t7 -gpuid 1
