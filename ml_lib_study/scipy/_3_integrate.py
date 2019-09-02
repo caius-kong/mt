6824b201b9b2e5d0092849e1a9bbc2d431b6319d
caius_kong <Caius_Kong@trendmicro.com>
2019-09-02 18:14:09 +0800

    scipy

--- ml_lib_study/scipy/_3_integrate.py
+++ ml_lib_study/scipy/_3_integrate.py
@@ -11,4 +11,7 @@ from numpy import exp
 def f(x): return exp(-x**2)
 
 
+# Quad函数是SciPy积分函数的主力。
+# 在a到b给定的固定范围内执行函数f(x)的单个积分的默认选择。
+# 四元函数返回两个值，其中第一个数字是积分值，第二个数值是积分值绝对误差的估计值。
 print(inte.quad(f, 0, 1))
